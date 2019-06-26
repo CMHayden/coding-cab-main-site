b12ccca047a4fea3b4d2956bb528acb6b8cf114c
CMHayden <haydencallum4@gmail.com>
2019-06-26 16:37:32 +0200

    add test for valid and invalid data

--- tests/Feature/ContactControllerTest.php
+++ tests/Feature/ContactControllerTest.php
@@ -10,28 +10,38 @@ class ContactControllerTest extends TestCase
 {
 
     public function test_post_contact_route () {
+
         $data =
             '{
                 "email": "haydencallum4@gmail.com",
                 "name": "Callum",
                 "message": "Hey! This is just a test message to ensure it works. Cheers, Callum"
             }';
+
         $response = $this->json('POST', 'api/contact',[
                 $data
             ]);
+
+            dd($response);
         $response->assertStatus(200);
+
     }
 
+
     public function test_empty_contact_form () {
+
         $data =
+
             '{
                 "email": "",
                 "name": "",
                 "message": ""
             }';
+
         $response = $this->json('POST', 'api/contact',[
                 $data
             ]);
-        $response->assertStatus(401);
+
+        $response->assertStatus(422);
     }
 }
