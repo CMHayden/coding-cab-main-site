ef44c53544d82de177d6df1675d8fa5ccd3fcf73
CMHayden <haydencallum4@gmail.com>
2019-06-26 14:48:01 +0200

    modify the contact view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,53 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
+            <div class="top-right links">
+                <a href="{{ url('/') }}">Home</a>
+            </div>
+            <section class="resume-section p-4 p-lg-5 text-center" id="contact">
+                    <div class="my-auto">
+                      <h2 class="mb-4 sec">Contact</h2>
 
+
+                      <form
+                        class="contact-form d-flex flex-column align-items-center"
+                        action="/contact"
+                        method="POST"
+                      >
+                        <div class="form-group w-75">
+                          <input
+                            type="name"
+                            class="form-control"
+                            placeholder="Name"
+                            name="name"
+                            required
+                          />
+                        </div>
+                        <div class="form-group w-75">
+                          <input
+                            type="email"
+                            class="form-control"
+                            placeholder="Email"
+                            name="name"
+                            required
+                          />
+                        </div>
+
+                        <div class="form-group w-75">
+                          <textarea
+                            class="form-control"
+                            type="text"
+                            placeholder="Message"
+                            rows="7"
+                            name="name"
+                            required
+                          ></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-submit btn-info w-75 btn-dark">Submit</button>
+                      </form>
+                    </div>
+                  </section>
     </body>
 </html>
 
